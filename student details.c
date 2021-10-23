5e6762e154c272a7259677e14849e8c495892554
Magaptisuresh <92911085+Magaptisuresh@users.noreply.github.com>
2021-10-23 12:37:35 +0530

    Add files via upload

---
+++ student details.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	printf("student\tpin\tage\tmarks\n");
+	printf("satish\t314\t20\t45\n");
+	printf("suresh\t349\t21\t55\n");
+	printf("bharath\t369\t535\t25\n");
+	printf("vamsi\t55\t55\t21\n");
+}
